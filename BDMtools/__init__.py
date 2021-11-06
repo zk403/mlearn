cc7a7ffc5fedb43f27c9463a56b6e73558f3db2f
Zengke <371825603@qq.com>
2021-11-06 17:27:29 +0800

    Update __init__.py

--- BDMtools/__init__.py
+++ BDMtools/__init__.py
@@ -1,17 +1,17 @@
 # -*- coding:utf-8 -*- 
 
-from .cleaner_cleaner import outliersTransformer,dtpypeAllocator,nanTransformer
-from .report_report import businessReport,EDAReport,varReport,varGroupsReport
-from .selector_bin import finbinSelector,optbinSelector
-from .selector_fa import faSelector
-from .selector_logtit import stepLogit,cardScorer
-from .selector_simple import preSelector,corrSelector
-from .selector_wrapper import RFECVSelector
-from .selector_embeded import lassoSelector,sequentialSelector
-from .encoder_woe import woeTransformer
-from .tuner_girdcv import girdTuner
-from .tuner_bayesian import BayesianXGBTuner,BayesianLgbmTuner
-from .selector_bin_fun import binAdjusterKmeans,binAdjusterChi
+from BDMtools.cleaner_cleaner import outliersTransformer,dtpypeAllocator,nanTransformer
+from BDMtools.report_report import businessReport,EDAReport,varReport,varGroupsReport
+from BDMtools.selector_bin import finbinSelector,optbinSelector
+from BDMtools.selector_fa import faSelector
+from BDMtools.selector_logtit import stepLogit,cardScorer
+from BDMtools.selector_simple import preSelector,corrSelector
+from BDMtools.selector_wrapper import RFECVSelector
+from BDMtools.selector_embeded import lassoSelector,sequentialSelector
+from BDMtools.encoder_woe import woeTransformer
+from BDMtools.tuner_girdcv import girdTuner
+from BDMtools.tuner_bayesian import BayesianXGBTuner,BayesianLgbmTuner
+from BDMtools.selector_bin_fun import binAdjusterKmeans,binAdjusterChi
 
 
 __version__ = '0.0.7'
