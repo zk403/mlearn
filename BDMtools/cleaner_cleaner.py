3a326befb01b7bfd3a3bc02641dc814c5b748330
zk403 <371825603@qq.com>
2021-12-16 10:47:51 +0800

    Update cleaner_cleaner.py

--- BDMtools/cleaner_cleaner.py
+++ BDMtools/cleaner_cleaner.py
@@ -106,7 +106,7 @@ class dtStandardization(TransformerMixin):
 class dtypeAllocator(TransformerMixin):
 
     
-    def __init__(self,dtypes_dict={},col_rm=None,t_unit='1 D',dtype_num='float64',drop_date=False):
+    def __init__(self,dtypes_dict={},col_rm=None,t_unit='1 D',dtype_num='float64',drop_date=False,precision=6):
         """ 
         列类型分配器：将原始数据中的列类型转换为适合进行数据分析与建模的数据类型，请注意
                   + 本模块不支持对complex、bytes类型的列进行转换
@@ -117,20 +117,21 @@ class dtypeAllocator(TransformerMixin):
             dtypes_dict={}
                 + dict():是否自动处理输入数据并最终转换为object、number、date三种类型
                     + 初始数据中的数值类型数据(float,int,bool)将被全部转换为float类型数据
-                    + 初始数据中的数值类型数据(str)将被全部转换为object类型数据
-                    + 初始数据中的数值类型数据(category-unordered)将被全部转换为object类型数据
-                    + 初始数据中的数值类型数据(category-ordered)将顺序被全部转换为int8类型数据(0,1,2,3...),其与原始数据的对应关系将被保存在self.order_info中
-                    + 初始数据中的数值类型数据(datetime,datetimetz)类型数据将保持默认,可通过参数选择是否剔除掉日期型数据
-                    + 初始数据中的数值类型数据(timedelta)类型数据将被转换为float,时间单位需自行指定,且作用于全部的timedelta类型
-                    + 其他类型的列与col_rm一致将不进行转换
+                    + 初始数据中的字符类型数据(str)将被全部转换为object类型数据
+                    + 初始数据中的无序分类类型数据(category-unordered)将被全部转换为object类型数据
+                    + 初始数据中的有序分类类型数据(category-ordered)将顺序被全部转换为int8类型数据(0,1,2,3...),其与原始数据的对应关系将被保存在self.order_info中
+                    + 初始数据中的时间类型数据(datetime,datetimetz)类型数据将保持默认,可通过参数选择是否剔除掉日期型数据
+                    + 初始数据中的时间差类型数据(timedelta)类型数据将被转换为float,时间单位需自行指定,且作用于全部的timedelta类型
+                    + 其他类型的列与col_rm列将不进行转换直接输出
                 + dict={'num':colname_list,'str':colname_list,'date':colname_list}
                     + colname_list是列名列表,可以为None,代表无此类特征,注意各个类的列名列表不能出现交集与重复,否则将报错终止
                     + 若所有colname_list的特征只是数据所有列的一部分，则剩下部分的列将不做转换
-                    + colname_list不能含有col_rm中的列,否则会出现错误
+                    + colname_list不能含有col_rm中的列,否则会报错终止
             col_rm=None or list,不参与转换的列的列名列表，其不会参与任何转换且最终会保留在输出数据中        
-            dtype_num='float64',数值类型列转换方式，默认为float64，可以选择float32/float16，注意其可以有效减少数据的内存占用，但会损失数据精度,请注意数据中的id列,建议不进行32或16转换
+            dtype_num='float64',数值类型列转换方式，默认为float64，可以选择float32/float16，注意其可以有效减少数据的内存占用，但会损失数据精度。请注意数据中的数值id列,建议不进行32或16转换
             t_unit=‘1 D’,timedelta类列处理为数值的时间单位，默认天
             drop_date=False,是否剔除原始数据中的日期列，默认False
+            precision=5,数值类数据的精度,precision=5代表保留小数点后5位小数，设定好设定此值以获得数值的近似结果，否则后续分析比如分箱、woe编码等会在一些极端条件下产生错误结果。
 
         Returns:
         ------
@@ -143,6 +144,7 @@ class dtypeAllocator(TransformerMixin):
         self.col_rm = col_rm
         self.drop_date=drop_date
         self.t_unit=t_unit
+        self.precision=precision
         
 
     def transform(self, X):
@@ -200,7 +202,7 @@ class dtypeAllocator(TransformerMixin):
                     
             X_keep=X.drop(columns,axis=1)
             
-            X_num=X[col_num].astype(self.dtype_num)
+            X_num=X[col_num].astype(self.dtype_num).apply(np.round,args=(self.precision,))
             
             X_obj=X[col_obj].astype('str')
             
@@ -222,7 +224,7 @@ class dtypeAllocator(TransformerMixin):
     def getXAuto(self,X):
       
         #数值
-        X_num=X.select_dtypes(include=['number','bool']).astype(self.dtype_num)        
+        X_num=X.select_dtypes(include=['number','bool']).astype(self.dtype_num).apply(np.round,args=(self.precision,))        
          
         #字符
         X_obj=X.select_dtypes(include=['object'])
@@ -247,7 +249,7 @@ class dtypeAllocator(TransformerMixin):
         X_date=None if self.drop_date else X.select_dtypes(include=['datetime','datetimetz'])
         
         #时间差
-        X_tdiff=X.select_dtypes(include=['timedelta']).div(pd.to_timedelta(self.t_unit)).astype(self.dtype_num)
+        X_tdiff=X.select_dtypes(include=['timedelta']).div(pd.to_timedelta(self.t_unit)).astype(self.dtype_num).apply(np.round,args=(self.precision,))
         
         #其他
         X_oth=X.select_dtypes(exclude=['number','bool','object','category','timedelta','datetime','datetimetz'])
@@ -324,7 +326,7 @@ class nanTransformer(TransformerMixin):
                       fill_value=(-9999,'missing'),  
                       n_neighbors=10,
                       weights_knn='uniform',
-                      indicator=False):
+                      indicator=False,dtype_num='float32'):
         """ 
         缺失值填补，集成sklearn.impute        
         注意本模块不支持除字符、数值以外（时间、日期、时间差类）列的填充，这些列将直接返回原始值
@@ -341,6 +343,7 @@ class nanTransformer(TransformerMixin):
         indicator:bool,是否生成缺失值指代特征
         n_neighbors:knn算法中的邻近个数k
         weights_knn:str,knn算法中的预测权重，可选‘uniform’, ‘distance’
+        dtype_num:str,填补后数值类型列的dtype
         
         Attributes
         ------
@@ -348,12 +351,13 @@ class nanTransformer(TransformerMixin):
         Examples
         ------        
         """
-        self.method=method
         self.missing_values=missing_values
+        self.method=method
         self.fill_value=fill_value
         self.indicator=indicator
         self.weights_knn=weights_knn
         self.n_neighbors=n_neighbors
+        self.dtype_num=dtype_num
 
         
     def fit(self,X, y=None):            
@@ -395,7 +399,7 @@ class nanTransformer(TransformerMixin):
                 
                 X_num_fill=pd.DataFrame(imputer_num.transform(X_num),
                                         columns=imputer_num.feature_names_in_,
-                                        index=X.index
+                                        index=X.index,dtype=self.dtype_num                                       
                                         ) 
             else:
                 
@@ -417,7 +421,7 @@ class nanTransformer(TransformerMixin):
                 
                 X_str_fill=pd.DataFrame(imputer_str.transform(X_str),
                                        columns=imputer_str.feature_names_in_,
-                                       index=X.index
+                                       index=X.index,dtype='str'
                                        ) 
             else:
                 
@@ -433,7 +437,7 @@ class nanTransformer(TransformerMixin):
                 indicator_na=MissingIndicator(missing_values=np.nan).fit(X[na_cols]) 
     
                 X_na=pd.DataFrame(indicator_na.transform(X[na_cols]),
-                                       columns=indicator_na.feature_names_in_,
+                                       columns=indicator_na.feature_names_in_,dtype='int8',
                                        index=X.index)
             else:
                 
