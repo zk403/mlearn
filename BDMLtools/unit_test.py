b439a054cb7b2d3481d6e94f391fecc7d53e26d8
zk403 <371825603@qq.com>
2022-07-12 10:00:21 +0800

    update files

---
+++ BDMLtools/unit_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 12 09:18:31 2022
+
+@author: zengke
+"""
+
+from BDMLtools import dtStandardization,outliersTransformer,dtypeAllocator,nanTransformer,\
+businessReport,prefitModel,EDAReport,varReportSinge,varReport,varGroupsReport,varGroupsPlot,\
+GainsTable,binSelector,binAdjuster,binFreq,binPretty,binTree,binChi2,binKmeans,BaseWoePlotter,faSelector,\
+stepLogit,cardScorer,preSelector,lassoSelector,LgbmSeqSelector,LgbmPISelector,LgbmShapRFECVSelector,\
+woeTransformer,gridTuner,hgridTuner,BayesianCVTuner,perfEval,perfEval2,shapCheck
+
+#code
